87c1e3826fe0c7048dd27008b4b5a88baa2707b1
Christin Banerjee <Christinprice5@gmail.com>
2020-07-22 17:13:57 -0400

    first test working

--- src/main/java/triangles/TriangleSorter.java
+++ src/main/java/triangles/TriangleSorter.java
@@ -19,18 +19,18 @@ public class TriangleSorter {
         if (sideA == sideB && sideB == sideC && sideC ==sideA){
             return "Equilateral";
         }
-        if (sideA ==sideB ) {
-            return "Isosceles";
-        }
-        if (printedNumber % 5 == 0) {
-            return "Right";
-        }
-        if (printedNumber % 5 == 0) {
-            return "Still a Triangle, but not one of the three.";
-        }
-        if (printedNumber % 5 == 0) {
-            return "Cannot be made into a triangle.";
-        }
-        return "" + printedNumber;
+//        if (sideA ==sideB ) {
+//            return "Isosceles";
+//        }
+//        if (printedNumber % 5 == 0) {
+//            return "Right";
+//        }
+//        if (printedNumber % 5 == 0) {
+//            return "Still a Triangle, but not one of the three.";
+//        }
+//        if (printedNumber % 5 == 0) {
+//            return "Cannot be made into a triangle.";
+//        }
+        return "";
     }
 }
