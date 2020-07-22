5fb43138b0a2600c19adfabb8cea56e2d4009902
Kyle Wang <kywang2@yahoo.com>
2020-07-22 18:12:56 -0400

    Created triangles app.

--- src/main/java/triangles/TriangleApp.java
+++ src/main/java/triangles/TriangleApp.java
@@ -6,7 +6,16 @@ import java.util.Scanner;
 
 public class TriangleApp {
     public static void main(String[] args) {
+        TriangleSorter triangleSorter = new TriangleSorter();
         Scanner keyboard = new Scanner(System.in);
+        System.out.println("Enter side A:");
+        int sideA = keyboard.nextInt();
+        System.out.println("Enter side B:");
+        int sideB = keyboard.nextInt();
+        System.out.println("Enter side C:");
+        int sideC = keyboard.nextInt();
+        System.out.println(triangleSorter.analyze(sideA, sideB, sideC));
+
 //Game Loop
 
     }
